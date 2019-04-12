56494fadb2d44b44627120e89cce14d2558826e1
acunago <49562365+acunago@users.noreply.github.com>
2019-04-12 15:09:40 -0300

    Initial commit

---
+++ Assets/Script/CameraCollision.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float minDistance = 1.0f;
+    public float maxDistance = 5.0f;
+    public float smooth = 10.0f;
+    Vector3 dollyDir;
+    public Vector3 dollyDirAdjusted;
+    public float distance;
+
+    // Use this for initialization
+    void Awake()
+    {
+        dollyDir = transform.localPosition.normalized;
+        distance = transform.localPosition.magnitude;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 desiredCameraPos = transform.parent.TransformPoint(dollyDir * maxDistance);
+        // (optional) put layers you don't want to collide with  here (probably things like enemies)
+        const int ignoreLayer1 = 1 ;
+        const int ignoreLayer2 = 2;
+        int layerMask = ignoreLayer1 | ignoreLayer2;
+        layerMask = ~layerMask;
+        RaycastHit hit;
+        if (Physics.Linecast(transform.parent.position, desiredCameraPos, out hit, layerMask))
+        {
+            distance = Mathf.Clamp(hit.distance, minDistance, maxDistance);
+        }
+        transform.localPosition = dollyDir * distance;
+    }
+}
---
+++ Assets/Script/CameraManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    /// <summary>
+    /// distancia en x del objetivo
+    /// </summary>
+    public float dstFromTarget = 2;
+    /// <summary>
+    /// Sensibilidad del mouse
+    /// </summary>
+    public float mouseSensitivity = 10;
+    /// <summary>
+    /// Suavisado de la sensibilidad
+    /// </summary>
+    public float rotationSmoothTime = .1f;
+    /// <summary>
+    /// Hasta donde baja y sube la camara
+    /// </summary>
+    public Vector2 pitchMinMax = new Vector2(-40, 85);
+    /// <summary>
+    /// bloquear mouse
+    /// </summary>
+    public bool lockCursor;
+    /// <summary>
+    /// Objetivo a seguir (CameraFollow)
+    /// </summary>
+    public Transform target;
+
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
+
+    float yaw;
+    float pitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+        pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+        transform.eulerAngles = currentRotation;
+
+        transform.position = target.position - transform.forward * dstFromTarget;
+    }
+}
